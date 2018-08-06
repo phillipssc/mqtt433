ceee701552778542e8c7618addfbeaeb9601f4ba
phillipssc <sean.c.phillips@gmail.com>
2018-08-06 10:32:09 -0400

    Add files via upload

--- mqtt433.py
+++ mqtt433.py
@@ -5,6 +5,7 @@ import json
 import paho.mqtt.client as mqtt
 import logging
 import subprocess
+import copy
 
 def on_connect(client, userdata, flags, rc):
     logging.info("CNNACK received with code %d." % (rc))
@@ -12,16 +13,20 @@ def on_connect(client, userdata, flags, rc):
 def on_publish(client, userdata, mid):
     logging.info("mid: "+str(mid))
 
-logging.basicConfig(filename=__file__.replace(".py",'.log'),level=logging.INFO)
+#logging.basicConfig(filename=__file__.replace(".py",'.log'),level=logging.INFO)
+logging.basicConfig(filename="/var/log/mqtt433.log",level=logging.INFO)
 logging.info("start")
 
 mqttc = mqtt.Client("python_pub")
-mqttc.username_pw_set(username="<MQTTUSERNAME>", password="<MQTTPASSWORD>")
+mqttc.username_pw_set(username="<LOGIN>", password="<PASSWORD>")
 mqttc.connect("localhost", port=1883)
 mqttc.loop_start()
 logging.info("main loop")
 
-proc = subprocess.Popen(['rtl_433', '-F', 'json', '-R', '11', '-R', '39'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+proc = subprocess.Popen(['rtl_433', '-F', 'json', '-R', '12', '-R', '30', '-R', '40'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#proc = subprocess.Popen(['rtl_433', '-F', 'json', '-G'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+linedata = {}
 
 while True:
     try:
@@ -29,12 +34,30 @@ while True:
 #        input = sys.stdin.readline()
         input = proc.stdout.readline()
         payload = json.loads(input.decode("utf-8"))
-        logging.debug(payload)
-        (rc, mid) = mqttc.publish("hass/rtl_433/sensor_" + str(payload['id']) , json.dumps(payload))
-#        (rc, mid) = mqttc.publish("hass/rtl_433", json.dumps(payload))
-        logging.debug("rc=%s, mid=%s" % (rc, mid))
-        mqttc.loop(2) # timeout = 2s
-        logging.debug("published")
+        # Make a copy for manipulaqtion
+        payload_nt = copy.copy(payload)
+        # Remove the time component (as it will often be the only changing value)        
+        payload_nt.pop('time')
+        logging.debug(json.dumps(payload_nt))
+        # Get the key for the storage dictionary
+        id = payload_nt['id']
+        logging.debug("id: " + str(id))
+        olddata = ""
+        # If we have previously stored data for this - load it into olddata
+        if id in linedata:
+            olddata = linedata[id]
+        logging.debug("old: "+json.dumps(olddata))
+        # If the data has changed for this id (other than time) send it (the original data) out
+        if payload_nt != olddata:
+            # For now - we will let openhab decode this
+        	(rc, mid) = mqttc.publish("home/rtl_433", json.dumps(payload))
+		    #(rc, mid) = mqttc.publish("home/rtl_433_2/sensor_" + str(payload['id']) , json.dumps(payload))
+		    #(rc, mid) = mqttc.publish("hass/rtl_433", json.dumps(payload))
+        	logging.debug("rc=%s, mid=%s" % (rc, mid))
+        	#mqttc.loop(2) # timeout = 2s
+        	logging.debug("published")
+        # Store the latest time-free data in the dictionary for this id
+        linedata[payload_nt['id']] = payload_nt
     except Exception as x:
         logging.warning("exception %s" % x)
         logging.warning("input:'%s'" % input)
